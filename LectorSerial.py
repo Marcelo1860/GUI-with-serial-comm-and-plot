8b18ce156b806cec941a965cbb721d129f9d1329
Marcelo1860 <chelobader@gmail.com>
2023-04-29 21:32:11 -0300

    Se genera interfaz grafica muy simple con QTdesigner

--- LectorSerial.py
+++ LectorSerial.py
@@ -1,16 +1,31 @@
-import serial
+
+import sys, serial, threading
+from PyQt6  import QtWidgets,uic
+from PyQt6.QtWidgets import *
 import time
-# Configurar el puerto serie
-ser = serial.Serial('COM4', 9600)
-time.sleep(2)
+import struct
 
-# Leer los datos del puerto serie
-while True:
-    try:
-        data = ser.readline().decode('ascii').strip()
-        print(data)
-    except KeyboardInterrupt:
-        break
+class arduino_read(QMainWindow):
+    def __init__(self,parent=None):
+        super(arduino_read,self).__init__(parent)
+        uic.loadUi('analogo.ui',self)
+        self.show()
+        self.pushButton.clicked.connect(lambda: threading.Thread(target = self.conexion).start())
 
-# Cerrar el puerto serie
-ser.close()
+    def conexion(self):
+        board = serial.Serial('COM4',baudrate = 9600)
+        time.sleep(2)
+
+        while True:
+            data1 = board.readline()
+            valor_limpio = data1.replace(b'\x00', b'').decode()
+            valor_limpio = valor_limpio.strip()
+            print(valor_limpio)
+            self.lcdNumber.display(valor_limpio)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlog = arduino_read()
+    dlog.show()
+    sys.exit(app.exec())
